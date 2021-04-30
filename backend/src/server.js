335f195b31b847fa98ac6d0cd7bb447669e329a6
luckrafael <lrs.rafael@hotmail.com>
2021-04-30 13:52:51 -0300

    consumindo o backend e fazendo validacoes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const UrlShortner = require("./models/index");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 function generateCode() {
   let text = "";
@@ -12,11 +15,16 @@ function generateCode() {
   return text;
 }
 
+const regExUrlValidation = /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gi;
+
 app.use(express.json());
 
-app.post("/", async (req, res, next) => {
+app.post("/shortner", async (req, res, next) => {
   try {
     const url = req.body.url;
+    if (url.length <= 0 || !regExUrlValidation.exec(url)) {
+      return res.status(400).send({ error: "Precisa de um URL valida" });
+    }
     const code = generateCode();
     await UrlShortner.create({ url, urlCode: code });
 
@@ -26,8 +34,16 @@ app.post("/", async (req, res, next) => {
   }
 });
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+app.get("/:urlCode", async (req, res) => {
+  try {
+    const urlCode = req.params.urlCode;
+    const urlFinal = await UrlShortner.findOne({ urlCode });
+    console.log(urlFinal);
+
+    return res.status(200).redirect(urlFinal.url);
+  } catch (err) {
+    return res.status(400).send({ error: "Failed to shorten url" });
+  }
 });
 
 app.listen(3333);
